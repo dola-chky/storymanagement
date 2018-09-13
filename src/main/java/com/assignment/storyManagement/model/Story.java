eb701d86dcb3c1034ec64ad86e63e17d385fe9cd
dola_chky <cdola@ymail.com>
2018-09-13 20:30:06 +0600

    Added property to enable h2 console and created model for story.

---
+++ src/main/java/com/assignment/storyManagement/model/Story.java
@@ -0,0 +1,62 @@
+package com.assignment.storyManagement.model;
+
+import com.assignment.storyManagement.Utils.JsonDateDeserializer;
+import com.assignment.storyManagement.Utils.JsonDateSerializer;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "story")
+@EntityListeners(AuditingEntityListener.class)
+public class Story {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long storyId;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String storyBody;
+
+    @Column(nullable = false)
+    private Date publishedDate;
+
+    public long getStoryId() {
+        return storyId;
+    }
+
+    public void setStoryId(long storyId) {
+        this.storyId = storyId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getStoryBody() {
+        return storyBody;
+    }
+
+    public void setStoryBody(String storyBody) {
+        this.storyBody = storyBody;
+    }
+
+    @JsonSerialize(using=JsonDateSerializer.class)
+    public Date getPublishedDate() {
+        return publishedDate;
+    }
+
+    @JsonDeserialize(using= JsonDateDeserializer.class)
+    public void setPublishedDate(Date publishedDate) {
+        this.publishedDate = publishedDate;
+    }
+}
