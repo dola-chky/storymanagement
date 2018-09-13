d3a7464f30416763761b59a928c07531190aab88
dola_chky <cdola@ymail.com>
2018-09-13 13:13:09 +0600

    initial commit of the repository with a spring starter project.

---
+++ src/main/java/com/assignment/storyManagement/StoryManagementApplication.java
@@ -0,0 +1,12 @@
+package com.assignment.storyManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StoryManagementApplication.class, args);
+	}
+}
